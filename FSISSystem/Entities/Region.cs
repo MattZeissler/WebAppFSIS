2e522337000d10c7769debd8d0702e0de6a40c48
Matthew Zeissler <mzeissler1@nait.ca>
2020-03-09 15:59:12 -0600

    V2

---
+++ FSISSystem/Entities/Region.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Threading.Tasks;
+
+namespace FSISSystem.Entities
+{
+    class Region
+    {
+    }
+}
